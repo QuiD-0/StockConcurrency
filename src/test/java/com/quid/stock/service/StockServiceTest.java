d33912637751e0cf8fed52078c48907863459603
QuiD-0 <wodnd101@naver.com>
2023-01-09 16:48:56 +0900

    멀티쓰레드 환경 테스트

--- src/test/java/com/quid/stock/service/StockServiceTest.java
+++ src/test/java/com/quid/stock/service/StockServiceTest.java
@@ -1,10 +1,14 @@
 package com.quid.stock.service;
 
+import static java.util.concurrent.Executors.newFixedThreadPool;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.quid.stock.domain.Stock;
 import com.quid.stock.repository.StockJpaRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,12 +41,34 @@ class StockServiceTest {
     @Test
     void decreaseStock() {
         stockService.decreaseStock(1L, 1L);
-        stockJpaRepository.findByProductId(1L).ifPresent(stock -> assertEquals(9L, stock.getQuantity()));
+        stockJpaRepository.findByProductId(1L)
+            .ifPresent(stock -> assertEquals(9L, stock.getQuantity()));
     }
 
     @Test
     void decreaseStock_when_quantity_is_bigger() {
         assertThrows(RuntimeException.class, () -> stockService.decreaseStock(1L, 100L));
+    }
+
+    @Test
+    void RaceCondition() throws InterruptedException {
+        int threadCount = 10;
+        ExecutorService executorService = newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decreaseStock(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        //여러 쓰레드에서 값을 동시에 변경할 경우 원하는 값이 나오지 않을 수 있다.
+        stockJpaRepository.findByProductId(1L)
+            .ifPresent(stock -> assertNotEquals(0L, stock.getQuantity()));
     }
 
 }
